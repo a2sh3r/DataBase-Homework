4dcdcb8aa140574836c71f70aead54cd02bfc65c
a2sh3r <ashurmetovmm@gmail.com>
2023-04-13 20:15:23 +0300

    Первое дз

--- lesson-1-jdbc/jdbc-demo-exercise/src/main/java/ru/mpei/spring/Main.java
+++ lesson-1-jdbc/jdbc-demo-exercise/src/main/java/ru/mpei/spring/Main.java
@@ -5,6 +5,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
 import ru.mpei.spring.dao.PersonDao;
+import ru.mpei.spring.domain.Person;
 
 @SpringBootApplication
 public class Main {
@@ -14,6 +15,14 @@ public class Main {
         ApplicationContext context = SpringApplication.run(Main.class);
 
         PersonDao dao = context.getBean(PersonDao.class);
+        System.out.println("All users count: \n"+ dao.count());
+        System.out.println("All users:\n" + dao.getAll());
+        System.out.println("Вставляем ивана:\n");
+        dao.insert(new Person(2, "vane4ka"));
+        System.out.println("All users count: \n"+ dao.count());
+        System.out.println("All users:\n" + dao.getAll());
+        Person vanka = dao.getById(2);
+        System.out.println("Id Vane4ki: " + vanka.getId() + " name : " + vanka.getName());
 
         Console.main(args);
     }
--- lesson-1-jdbc/jdbc-demo-exercise/src/main/java/ru/mpei/spring/dao/PersonDaoJdbc.java
+++ lesson-1-jdbc/jdbc-demo-exercise/src/main/java/ru/mpei/spring/dao/PersonDaoJdbc.java
@@ -8,10 +8,11 @@ import org.springframework.jdbc.core.namedparam.NamedParameterJdbcOperations;
 import org.springframework.stereotype.Repository;
 import ru.mpei.spring.domain.Person;
 
-import java.util.List;
+import java.util.*;
 
 @Repository
 public class PersonDaoJdbc implements PersonDao {
+
     private final NamedParameterJdbcOperations jdbc;
 
     public PersonDaoJdbc(NamedParameterJdbcOperations jdbcOperations) {
@@ -20,35 +21,45 @@ public class PersonDaoJdbc implements PersonDao {
 
     @Override
     public int count() {
-        return jdbc.getJdbcOperations()
-            .queryForObject("select count(*) from persons", Integer.class);
+        Integer count = jdbc.getJdbcOperations()
+                .queryForObject("select count(*) from persons", Integer.class);
+        return count == null? 0: count;
     }
 
     @Override
     public void insert(Person person) {
-
+        jdbc.update("insert into persons (id, name) values (:id, :name)",
+                Map.of("id", person.getId(), "name", person.getName()));
     }
 
     @Override
     public Person getById(long id) {
-        return null;
+        var params = Map.of("id", id);
+        return jdbc.queryForObject(
+                "select id, name from persons where id = :id", params, new PersonMapper()
+        );
     }
 
     @Override
     public List<Person> getAll() {
-        return null;
+        return jdbc.query("select * from persons", new PersonMapper());
     }
 
     @Override
     public void deleteById(long id) {
-
+        var params = Map.of("id", id);
+        jdbc.update(
+                "delete from persons where id = :id", params
+        );
     }
 
     private static class PersonMapper implements RowMapper<Person> {
 
         @Override
         public Person mapRow(ResultSet resultSet, int i) throws SQLException {
-            return null;
+            long id = resultSet.getLong("id");
+            String name = resultSet.getString("name");
+            return new Person(id, name);
         }
     }
 }
